5bc5825b1cc0f66f22b264b22f09395cf8cadcda
COValhalla <josephvellella@proton.me>
2022-11-11 10:26:39 -0700

    Added SVG for back.

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -29,7 +29,20 @@ function Buttons({ buttonUpdate }) {
             type="button"
             className="h-10 w-10 rounded-md border  bg-red-700 hover:bg-red-600"
           >
-            Back
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="m-auto h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z"
+              />
+            </svg>
           </button>
           <button
             onClick={() => buttonUpdate('7')}

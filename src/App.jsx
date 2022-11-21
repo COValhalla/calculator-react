1f94170c1f2227daae619fafa318ea020e3cb0d5
COValhalla <josephvellella@proton.me>
2022-11-20 20:57:14 -0700

    Update dep.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import React from 'react'
+
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Our First Test</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
